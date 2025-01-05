b73e7206e68b20a22d4d2d47ceb88e14fab1bb30
bearatron <84403830+bearatron@users.noreply.github.com>
2025-01-04 22:44:53 -0500

    Add text box latex inputs

--- src/SideBar.jsx
+++ src/SideBar.jsx
@@ -2,53 +2,119 @@ import "./SideBar.css";
 import { useState } from "react";
 import { parseTex } from "tex-math-parser";
 import * as math from "mathjs";
-import { addStyles, EditableMathField } from "react-mathquill";
+import { addStyles, StaticMathField, EditableMathField } from "react-mathquill";
+import InputGroup from "./InputGroup";
+import ErrorContainer from "./ErrorContainer";
 
 addStyles();
 
 export default function SideBar() {
-  const [latex, setLatex] = useState("");
-  const [equationVar, setEquationVar] = useState(0);
+  const [func, setFunc] = useState("");
+  const [funcError, setFuncError] = useState("");
+  const [lowerBound, setLowerBound] = useState("");
+  const [lowerBoundError, setLowerBoundError] = useState("");
+  const [upperBound, setUpperBound] = useState("");
+  const [upperBoundError, setUpperBoundError] = useState("");
+  const [axisOfRotation, setAxisOfRotation] = useState("x");
+
+  const [varText, setVarText] = useState("");
+  const [varError, setVarError] = useState("");
   const [ansText, setAnsText] = useState("");
-  const [errorMsg, setErrorMsg] = useState("");
 
   function handleSubmit() {
     console.log("button pressed!");
-    console.log(typeof equationVar);
 
-    const node = parseTex(latex);
-    const code = node.compile();
-    const mathJSAns = code.evaluate({ x: equationVar });
-    console.log(mathJSAns);
+    const funcNode = parseTex(func);
+    const funcCode = funcNode.compile();
+
+    const varNode = parseTex(varText);
+    const varCode = varNode.compile();
+    const varValue = varCode.evaluate();
+
+    console.log(varValue);
+    const mathJSAns = funcCode.evaluate({ x: varValue });
 
     setAnsText(mathJSAns.toString());
   }
 
-  function handleVarChange(e) {
-    const val = Number(e.target.value);
-    if (isNaN(val)) {
-      setErrorMsg("Please enter a number");
-    } else {
-      setEquationVar(val);
-      setErrorMsg("");
-    }
-  }
+  function handleVarChange(mathField) {
+    setVarText(mathField.latex());
 
-  function handleFunctionChange(mathField) {
-    setLatex(mathField.latex());
+    // const node = parseTex(varText);
+    // const code = node.compile();
+    // setVarValue(code.evaluate());
+    // setVarError("");
+
+    // const val = Number(e.target.value);
+    // if (isNaN(val)) {
+    //   setVarError("Please enter a number");
+    // } else {
+    //   setVarValue(val);
+    //   setVarError("");
+    // }
   }
 
   return (
     <div className="sidebar">
       <h1>Volume Visualizer</h1>
 
-      <EditableMathField latex={latex} onChange={handleFunctionChange} />
+      {
+        //TODO: Refactor
+        /* <InputGroup
+        labelTexStr="f(x)\\space=\\space"
+        varInput={true}
+        inputTexStr={func}
+        setInputTexStr={setFunc}
+        errorText={funcError}
+      /> */
+      }
 
-      <p>LaTeX Output:</p>
-      <p>{latex}</p>
-
+      <div className="input-group">
+        <StaticMathField className="input-label">
+          {"f(x)\\space=\\space"}
+        </StaticMathField>
+        <EditableMathField
+          latex={func}
+          className="math-input"
+          onChange={(mathField) => {
+            setFunc(mathField.latex());
+          }}
+        />
+      </div>
+      <ErrorContainer>{funcError}</ErrorContainer>
+      <div className="input-group">
+        <StaticMathField>{"a\\space=\\space"}</StaticMathField>
+        <EditableMathField
+          latex={lowerBound}
+          className="math-input"
+          onChange={(mathField) => {
+            setLowerBound(mathField.latex());
+          }}
+        />
+      </div>
+      <ErrorContainer>{lowerBoundError}</ErrorContainer>
+      <div className="input-group">
+        <StaticMathField>{"b\\space=\\space"}</StaticMathField>
+        <EditableMathField
+          latex={upperBound}
+          className="math-input"
+          onChange={(mathField) => {
+            setUpperBound(mathField.latex());
+          }}
+        />
+      </div>
+      <ErrorContainer>{upperBoundError}</ErrorContainer>
       <hr />
       <div className="input-group">
+        <StaticMathField>{"x\\space=\\space"}</StaticMathField>
+        <EditableMathField
+          latex={varText}
+          className="math-input"
+          onChange={handleVarChange}
+        />
+      </div>
+      <ErrorContainer>{varError}</ErrorContainer>
+      {/* <div className="input-group">
         <p>x:</p>
         <input
           type="text"
@@ -57,10 +123,8 @@ export default function SideBar() {
           onChange={handleVarChange}
         />
       </div>
-      {<p>{errorMsg}</p> ? errorMsg : <></>}
-
+      {<p>{varError}</p> ? varError : <></>} */}
       <button onClick={handleSubmit}>Submit</button>
-
       {<p>Answer: {ansText}</p> ? ansText : <></>}
     </div>
   );
