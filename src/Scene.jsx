20265d2b022934644604fc36d5ca729b31d4dd22
bearatron <84403830+bearatron@users.noreply.github.com>
2025-01-10 13:33:17 -0500

    Start adding 3d stuff

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -1,5 +1,44 @@
 import "./Scene.css";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { useState, useEffect, useRef } from "react";
 
 export default function Scene() {
-  return <div className="scene">Lorem ipsum</div>;
+  const sceneRef = useRef(null);
+
+  const [size, setSize] = useState({
+    width: sceneRef.clientWidth,
+    height: sceneRef.clientWidth,
+  });
+
+  // Update canvas size on window resize
+  useEffect(() => {
+    const handleResize = () => {
+      setSize({ width: sceneRef.clientWidth, height: sceneRef.clientWidth });
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Clean up the event listener
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <div ref={sceneRef} className="scene-container">
+      <Canvas>
+        {/* <color attach="background" args={["black"]} /> */}
+
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[10, 10, 5]} intensity={1} />
+
+        <mesh position={[0, 0, 0]}>
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color="orange" />
+        </mesh>
+        <OrbitControls />
+      </Canvas>
+    </div>
+  );
 }
