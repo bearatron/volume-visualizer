b97c3cf6c151c5d1b66642c006be98bf003aa797
bearatron <84403830+bearatron@users.noreply.github.com>
2025-01-11 22:09:52 -0500

    Add sidebar toggle

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Scene from "./threejsComponents/Scene";
 import SideBar from "./SideBar";
 import { useState } from "react";
 import { YAXIS } from "./threejsComponents/utils";
+import { FiSidebar } from "react-icons/fi";
+import { motion, AnimatePresence } from "motion/react";
 
 export default function App() {
   const [f, setF] = useState(() => (x) => x ** 2);
@@ -11,15 +13,36 @@ export default function App() {
   const [max, setMax] = useState(1);
   const [globalRotationAxis, setGlobalRotationAxis] = useState(YAXIS);
 
+  const [sideBarVisible, setSideBarVisible] = useState(true);
+
   return (
     <div className="App">
-      <SideBar
-        setF={setF}
-        setG={setG}
-        setMin={setMin}
-        setMax={setMax}
-        setGlobalRotationAxis={setGlobalRotationAxis}
-      />
+      <motion.button
+        whileTap={{ scale: 0.95 }}
+        className="sidebar-toggle"
+        onClick={() => setSideBarVisible(!sideBarVisible)}
+      >
+        <FiSidebar size={20} className="sidebar-icon" />
+      </motion.button>
+      <AnimatePresence initial={false}>
+        {sideBarVisible ? (
+          <motion.div
+            initial={{ opacity: 1, x: -100 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 1, x: -100 }}
+          >
+            <SideBar
+              setF={setF}
+              setG={setG}
+              setMin={setMin}
+              setMax={setMax}
+              setGlobalRotationAxis={setGlobalRotationAxis}
+            />
+          </motion.div>
+        ) : (
+          <></>
+        )}
+      </AnimatePresence>
       <Scene
         f={f}
         g={g}
